3fabd40442b7d6395be72247c61664bd24e765b9
bfddeveloper <bdicksondev@gmail.com>
2023-11-01 10:02:36 -0500

    Initial Commit

---
+++ Craps/CrapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrapsApp.swift
+//  Craps
+//
+//  Created by Brody Dickson on 11/1/23.
+//
+
+import SwiftUI
+
+@main
+struct CrapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
