94a8cdc802e96e5f682b3abc7e2ca05320d9ddd5
bfddeveloper <bdicksondev@gmail.com>
2023-11-06 09:52:29 -0600

    made player count control

--- Craps/Title screen.swift
+++ Craps/Title screen.swift
@@ -8,8 +8,40 @@
 import SwiftUI
 
 struct Title_screen: View {
+    @State var PlayerCount = 1
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Text("LRC")
+                .font(.largeTitle)
+            Text("Players")
+                .font(.largeTitle)
+            
+            HStack{
+                Image("Arrow")
+                    .resizable()
+                    .frame(width: 75, height: 75)
+                    .rotationEffect(Angle(degrees: 180))
+                    .onTapGesture {
+                        if PlayerCount > 1 {
+                            PlayerCount -= 1
+                        }
+                    }
+                Text(String(PlayerCount))
+                    .font(.largeTitle)
+                    .frame(width: 75, height: 75)
+                Image("Arrow")
+                    .resizable()
+                    .frame(width: 75, height: 75)
+                    .onTapGesture {
+                        if PlayerCount < 9 {
+                            PlayerCount += 1
+                        }
+                    }
+            }
+            Button("How to play"){
+                
+            }
+        }
     }
 }
 
